c303fe0c071fd19e9c8551c9d9cc76a5fc696d9d
Abdul Wasy <abdulwasey911@gmail.com>
2019-10-28 22:34:06 +0500

    Use Custom Dialogs  - Create an AlertDialog or alerting the User.  - Create LogDialog for logging the error Message  - Create the DialogExtentions/CustomDialog.cs for extentions methods  - Use the IDialog Interface.  - Use the ILogger for logging.
    fixes #14

---
+++ src/DevAssessment/DialogExtentions/CustomDialog.cs
@@ -0,0 +1,41 @@
+﻿using DevAssessment.CustomDialogs;
+using Prism.Services.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DevAssessment.DialogExtentions
+{
+    public static class CustomDialog
+    {
+        public const string keyAlertMessage = "AlertMessage";
+        public const string KeyLogMessage = "LogMessage";
+
+        
+        public static void DisplayAlert(this IDialogService dialogService, string message)
+        {
+            var param = new DialogParameters();
+            param.Add(keyAlertMessage, message);
+            
+            dialogService.ShowDialog(typeof(AlertDialog).Name,param, CloseDialog);
+        }
+
+        public static void DisplayError(this IDialogService dialogService, Exception ex, string message = null)
+        {
+            var param = new DialogParameters();
+            param.Add(KeyLogMessage, ex);
+
+            dialogService.ShowDialog(typeof(LogDialog).Name, param , CloseLogDialog );
+        }
+
+        private static void CloseDialog(IDialogResult dialogResult)
+        {
+
+        }
+
+        private static void CloseLogDialog(IDialogResult dialogResult)
+        {
+
+        }
+    }
+}
