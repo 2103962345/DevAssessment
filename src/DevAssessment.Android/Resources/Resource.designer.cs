1f4fe5a72974206a22449540423e9dede5dd520a
Abdul Wasy <abdulwasey911@gmail.com>
2019-10-28 16:34:31 +0500

    Add a Splash Screen for Android  - Add a style(Theme.Splash) for Splash Screen in styles.xml  - Modified the MainActivity Class and update the Theme style to Theme.Splash  - Add a splash_icon.png for splash screen in drawable.  - Add a splash_screen.xml in drawable  - set the bitmap source to "splash_icon" in splash_screen.xml.
    fixes #13

--- src/DevAssessment.Android/Resources/Resource.designer.cs
+++ src/DevAssessment.Android/Resources/Resource.designer.cs
@@ -8403,13 +8403,16 @@ namespace DevAssessment.Droid
 			public const int profile_avatar = 2131165303;
 			
 			// aapt resource value: 0x7F070078
-			public const int splash_screen = 2131165304;
+			public const int splash_icon = 2131165304;
 			
 			// aapt resource value: 0x7F070079
-			public const int tooltip_frame_dark = 2131165305;
+			public const int splash_screen = 2131165305;
 			
 			// aapt resource value: 0x7F07007A
-			public const int tooltip_frame_light = 2131165306;
+			public const int tooltip_frame_dark = 2131165306;
+			
+			// aapt resource value: 0x7F07007B
+			public const int tooltip_frame_light = 2131165307;
 			
 			static Drawable()
 			{
